8da010c97617f68b9d25591490c93e1dce586e70
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-07 17:07:56 +0700

    Update startminer.sh

--- startminer.sh
+++ startminer.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+./hellminer -c stratum+ssl://na.luckpool.net:3958 -u RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.r1 -p x --cpu 72
