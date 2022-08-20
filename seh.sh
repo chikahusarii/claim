a960576c80d53ced6caff33e1c40ed284e065184
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-21 00:23:29 +0700

    Create seh.sh

---
+++ seh.sh
@@ -0,0 +1,6 @@
+sudo apt update
+apt install git wget nano libpci-dev
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
+tar xf lolMiner_v1.31_Lin64.tar.gz
+cd 1.31
+./lolMiner --algo ETHASH --pool 34.127.80.91:5688 --user TRX:TBbwEfGT2h9iXXPbu1p9bgbjKK8YqegfYM.z1
